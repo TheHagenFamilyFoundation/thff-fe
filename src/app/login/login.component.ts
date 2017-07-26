b9177ba240f2c6c7d266c46e8a9d0bf5c5940b4b
Logan Born <lcborn4@gmail.com>
2017-07-25 22:17:41 -0400

    added the titles to the home, login and register components

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  title = "Login"
+
   constructor() { }
 
   ngOnInit() {
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
+  title = "Register"
+
   constructor() { }
 
   ngOnInit() {
