a2bb137b8fd2a69c559406e071b85995549f74c8
Logan Born <lcborn4@gmail.com>
2017-08-13 17:48:15 -0400

    removed the app from teh selector

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map'
+
 
 @Component({
-  selector: 'app-register',
+  selector: 'register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
@@ -9,9 +13,15 @@ export class RegisterComponent implements OnInit {
 
   title = "Register"
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  register(): void {
+    console.log("You clicked on the Register")
+
+    this.router.navigate(['/login']);
   }
 
 }
