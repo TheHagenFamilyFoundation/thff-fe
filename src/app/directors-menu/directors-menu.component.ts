5fb72b3ef570b6c6d8e52283cddcf24afc253872
Logan Born <lcborn4@gmail.com>
2017-08-13 15:12:42 -0400

    created directors-menu component

---
+++ src/app/directors-menu/directors-menu.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-directors-menu',
+  templateUrl: './directors-menu.component.html',
+  styleUrls: ['./directors-menu.component.css']
+})
+export class DirectorsMenuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
