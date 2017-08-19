09bfead9926a96f9fbaf711b211e07652f6ab782
Logan Born <lcborn4@gmail.com>
2017-08-19 17:35:15 -0400

    added in the post to api and receive token

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,19 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Http } from '@angular/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import 'rxjs/add/operator/map'
 
 
 @Component({
   selector: 'register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
+  providers: [HttpClient]
 })
 export class RegisterComponent implements OnInit {
 
-  title = "Register"
+  title = "Register";
 
-  constructor(private router: Router) { }
+  //API_URL = 'https://hagenfoundationbackend.herokuapp.com'
+
+  API_URL = 'http://localhost:1337';
+  //https://hagenfoundationbackend.herokuapp.com/requests
+  //API_URL = 'localhost:1337';
+
+  body;
+
+  userName;
+  email;
+  password;
+  confirmPassword;
+
+  data;
+  results: any;
+
+  constructor(private router: Router, private http: HttpClient) { }
 
   ngOnInit() {
   }
@@ -21,7 +38,81 @@ export class RegisterComponent implements OnInit {
   register(): void {
     console.log("You clicked on the Register")
 
-    this.router.navigate(['/login']);
+    let urlString = this.API_URL + '/user';
+
+    console.log("userName = " + this.userName);
+    console.log("email = " + this.email);
+    console.log("password = " + this.password);
+    console.log("confirmPassword = " + this.confirmPassword);
+
+    this.body = {
+      username: this.userName,
+      email: this.email,
+      password: this.password,
+      confirmPassword: this.confirmPassword
+    }
+
+    console.log(this.body);
+    console.log(urlString);
+
+    // this.http.get(this.API_URL + '/foo')
+    //   .subscribe(data => {
+    //     //this.results = data['results'];
+    //     console.log(data);
+    //     //console.log(this.results);
+    //   }
+    //   );
+
+
+    // let headers = new HttpHeaders({
+    //   'Access-Control-Allow-Origin': '*', "Access-Control-Allow-Credentials": "true",
+    //   "Access-Control-Allow-Methods": "GET,HEAD,OPTIONS,POST,PUT, DELETE",
+    //   "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    // });
+    //headers.append("Access-Control-Allow-Credentials", "true", )
+    //headers.append("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    //headers.append("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+
+    //console.log(headers);
+
+
+    // this.http.get(this.API_URL + '/requests') //, { headers: headers } 
+    //   //)
+    //   //.subscribe(data => data, //{
+    //   // Read the result field from the JSON response.
+    //   //this.results = data['results'];
+
+    //   .subscribe(data => console.log(data)
+
+
+    //   //  console.log(this.results);
+
+
+    //   //  }
+    //   );
+
+
+    //send to api
+
+    this.http.post(urlString, this.body)
+      .subscribe(data => {
+        this.results = data;
+        console.log(data);
+        console.log("this.results");
+        console.log(this.results);
+        console.log(this.results.token);
+
+        //console.log(this.results.token)
+        localStorage.setItem('token', this.results.token);
+
+        console.log("token = " + localStorage.getItem('token'));
+
+      }
+      );
+
+
+    //this.router.navigate(['/login']);
+    this.router.navigate(['/home']);
   }
 
 }
