ff1702743c1a22d2a0758b5b5de972a5371d34b6
Logan Born <lcborn4@gmail.com>
2017-08-20 17:32:57 -0400

    added auth service to the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { AuthService } from './auth/auth.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private authService: AuthService) {
+  }
+
+
 }
