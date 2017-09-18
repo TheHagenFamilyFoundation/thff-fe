5d78295efd09bc8c340e80dc5ef34164f5a291ee
Logan Born <lcborn4@gmail.com>
2017-09-17 22:12:31 -0400

    added the file path for the grant request process image

--- src/app/home/application-materials/process/process.component.ts
+++ src/app/home/application-materials/process/process.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProcessComponent implements OnInit {
 
+  fullImagePath = '/assets/images/grantreqproc.gif';
+
+
   constructor() { }
 
   ngOnInit() {
