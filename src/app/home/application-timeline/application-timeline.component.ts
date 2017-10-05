8b250ad4cf99f2db7ce54d3b44f438763475f4df
Logan Born <lcborn4@gmail.com>
2017-10-05 15:35:36 -0400

    created dynamic date for the table

--- src/app/home/application-timeline/application-timeline.component.ts
+++ src/app/home/application-timeline/application-timeline.component.ts
@@ -13,11 +13,40 @@ export class ApplicationTimelineComponent {
   displayedColumns = ['Step', 'Date'];
   dataSource = new ExampleDataSource();
 
+  currentYear: number;
+  nextYear: number;
+
   cycleDate: any;// = "2017-09-30";// = new Date();
+  dd: any;
+  mm: any;
+  today: any;
+  yyyy: any;
 
   constructor() {
 
-    this.cycleDate = '2017-09-30';
+    this.today = new Date();
+    this.dd = this.today.getDate();
+    this.mm = this.today.getMonth() + 1; //January is 0!
+    this.yyyy = this.today.getFullYear();
+
+    if (this.dd < 10) {
+      this.dd = '0' + this.dd
+    }
+
+    if (this.mm < 10) {
+      this.mm = '0' + this.mm
+    }
+
+    this.today = this.mm + '/' + this.dd + '/' + this.yyyy;
+    this.cycleDate = this.today;
+
+    //this.cycleDate = '2017-09-30';
+    //this.cycleDate = new Date();
+    //this.currentYear = 2017;
+    this.currentYear = this.yyyy;
+    //this.nextYear = 2018;
+    this.nextYear = this.yyyy + 1;
+    //console.log(this.currentYear)
 
   }
 
@@ -28,8 +57,15 @@ export interface Element {
   Date: string;
 }
 
+const today = new Date();
+const currentYear = today.getFullYear();
+
 const data: Element[] = [
-  { Step: '', Date: 'Hydrogen' },
+  {
+    Step: `Deadline for Letters of Intent\
+    If your organization wishes to apply for funding, you must begin by completing the Letter of Intent.`
+    , Date: `Submitted by: April ` + currentYear
+  },
   { Step: '', Date: 'Helium' },
   { Step: '', Date: 'Lithium' },
   { Step: '', Date: 'Beryllium' },
