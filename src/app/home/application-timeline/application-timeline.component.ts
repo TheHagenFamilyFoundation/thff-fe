a67aa1c54d9bf312e88f20e2ebd3c98491121df6
Logan Born <lcborn4@gmail.com>
2017-10-05 17:53:18 -0400

    added a previousYear variable

--- src/app/home/application-timeline/application-timeline.component.ts
+++ src/app/home/application-timeline/application-timeline.component.ts
@@ -11,10 +11,10 @@ import 'rxjs/add/observable/of';
 export class ApplicationTimelineComponent {
 
   displayedColumns = ['Step', 'Date'];
-  dataSource = new ExampleDataSource();
 
   currentYear: number;
   nextYear: number;
+  previousYear: number;
 
   cycleDate: any;// = "2017-09-30";// = new Date();
   dd: any;
@@ -46,47 +46,11 @@ export class ApplicationTimelineComponent {
     this.currentYear = this.yyyy;
     //this.nextYear = 2018;
     this.nextYear = this.yyyy + 1;
+    this.previousYear = this.yyyy - 1;
     //console.log(this.currentYear)
 
   }
 
 }
 
-export interface Element {
-  Step: string;
-  Date: string;
-}
 
-const today = new Date();
-const currentYear = today.getFullYear();
-
-const data: Element[] = [
-  {
-    Step: `Deadline for Letters of Intent\
-    If your organization wishes to apply for funding, you must begin by completing the Letter of Intent.`
-    , Date: `Submitted by: April ` + currentYear
-  },
-  { Step: '', Date: 'Helium' },
-  { Step: '', Date: 'Lithium' },
-  { Step: '', Date: 'Beryllium' },
-  { Step: '', Date: 'Boron' },
-  { Step: '', Date: 'Carbon' },
-  { Step: '', Date: 'Nitrogen' },
-  { Step: '', Date: 'Oxygen' },
-
-];
-
-/**
- * Data source to provide what data should be rendered in the table. The observable provided
- * in connect should emit exactly the data that should be rendered by the table. If the data is
- * altered, the observable should emit that new set of data on the stream. In our case here,
- * we return a stream that contains only one set of data that doesn't change.
- */
-export class ExampleDataSource extends DataSource<any> {
-  /** Connect function called by the table to retrieve one stream containing the data to render. */
-  connect(): Observable<Element[]> {
-    return Observable.of(data);
-  }
-
-  disconnect() { }
-}
