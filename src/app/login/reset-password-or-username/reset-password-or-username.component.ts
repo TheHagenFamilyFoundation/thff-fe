65eada8207eb651f284d1d73b7b7ae3897eb0893
Logan Born <lcborn4@gmail.com>
2017-10-05 21:40:24 -0400

    had an extra s in the reset-password-or-username

--- src/app/login/reset-password-or-username/reset-password-or-username.component.ts
+++ src/app/login/reset-password-or-username/reset-password-or-username.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-reset-password-or-username',
   templateUrl: './reset-password-or-username.component.html',
-  styleUrls: ['./resets-password-or-username.component.css']
+  styleUrls: ['./reset-password-or-username.component.css']
 })
 export class ResetPasswordOrUsernameComponent implements OnInit {
 
