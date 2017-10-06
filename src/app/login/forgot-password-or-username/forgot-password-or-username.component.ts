9bec8656fc08a61de2568dd4d03187b5fea8e15b
Logan Born <lcborn4@gmail.com>
2017-10-05 21:05:27 -0400

    created the forgot-password-or-username component

---
+++ src/app/login/forgot-password-or-username/forgot-password-or-username.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forgot-password-or-username',
+  templateUrl: './forgot-password-or-username.component.html',
+  styleUrls: ['./forgot-password-or-username.component.css']
+})
+export class ForgotPasswordOrUsernameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
