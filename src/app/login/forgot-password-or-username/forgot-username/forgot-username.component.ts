af5a44a4473b137abc141cccfe7120dda76714ce
Logan Born <lcborn4@gmail.com>
2017-10-05 21:16:23 -0400

    added the title to the forgot username component

--- src/app/login/forgot-password-or-username/forgot-username/forgot-username.component.ts
+++ src/app/login/forgot-password-or-username/forgot-username/forgot-username.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ForgotUsernameComponent implements OnInit {
 
+  title = "Forgot Username"
+
   constructor() { }
 
   ngOnInit() {
