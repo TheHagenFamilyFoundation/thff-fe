3c54127f3ae9d80e11c39581f71020638f8086c6
Logan Born <lcborn4@gmail.com>
2017-10-05 21:14:16 -0400

    created the forgot-username component

---
+++ src/app/login/forgot-password-or-username/forgot-username/forgot-username.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ForgotUsernameComponent } from './forgot-username.component';
+
+describe('ForgotUsernameComponent', () => {
+  let component: ForgotUsernameComponent;
+  let fixture: ComponentFixture<ForgotUsernameComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ForgotUsernameComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ForgotUsernameComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/login/forgot-password-or-username/forgot-username/forgot-username.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-forgot-username',
+  templateUrl: './forgot-username.component.html',
+  styleUrls: ['./forgot-username.component.css']
+})
+export class ForgotUsernameComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
