32d7dbc034f49b41a22b3c15399bc995e7c305f2
Logan Born <lcborn4@gmail.com>
2017-10-05 20:50:39 -0400

    added a snackbar to the auth.guard

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -1,17 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
- 
+import { MdSnackBar } from '@angular/material';
+
 @Injectable()
 export class AuthGuard implements CanActivate {
- 
-    constructor(private router: Router) { }
- 
+
+    constructor(private router: Router, public snackBar: MdSnackBar) {
+
+        console.log("help - auth guard");
+
+    }
+
     canActivate() {
+
+        console.log("this is the canActivate function");
+
         if (localStorage.getItem('currentUser')) {
             // logged in so return true
             return true;
         }
- 
+
+        // Simple message.
+        let snackBarRef = this.snackBar.open('You need to be Loggged In to access that page', 'OK', {
+            duration: 3000
+        });
+
         // not logged in so redirect to login page
         this.router.navigate(['/login']);
         return false;
