aca843dbb886a2af4a22c5e8179230d59bcaf4dc
Logan Born <lcborn4@gmail.com>
2017-10-05 21:38:15 -0400

    new file- reset-username componetn

---
+++ src/app/login/reset-password-or-username/reset-username/reset-username.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Reset-username',
+  templateUrl: './Reset-username.component.html',
+  styleUrls: ['./Reset-username.component.css']
+})
+export class ResetUsernameComponent implements OnInit {
+
+  title = "Reset Username"
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
