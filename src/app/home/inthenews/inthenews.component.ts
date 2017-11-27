c5e69251d3b02cf6836f0a2880a2e89152e9d272
Logan Born <lcborn4@gmail.com>
2017-11-27 00:16:39 -0500

    no neeed for the document t scroll to instead just need the window.scrollTo(0,0) to go to the top

--- src/app/home/inthenews/inthenews.component.ts
+++ src/app/home/inthenews/inthenews.component.ts
@@ -43,9 +43,6 @@ export class InthenewsComponent implements OnInit {
 
     console.log('topFunction')
 
-    document.body.scrollTop = 0; // For Safari
-    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-
     window.scrollTo(0, 0) //scroll back up to the top
 
   }
