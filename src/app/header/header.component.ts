77c6cc834a6e8ee8e26c30335ef0fead59456062
Logan Born <lcborn4@gmail.com>
2017-11-26 19:40:54 -0500

    added the auth service for the header

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AuthService } from '../auth/auth.service';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public authService: AuthService) { }
 
   ngOnInit() {
   }
