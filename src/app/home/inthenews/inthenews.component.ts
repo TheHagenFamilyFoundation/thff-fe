d66120eec5b659358e10bf1abadb5e3656f1a544
Logan Born <lcborn4@gmail.com>
2017-11-27 00:11:17 -0500

    added the windows onscroll

--- src/app/home/inthenews/inthenews.component.ts
+++ src/app/home/inthenews/inthenews.component.ts
@@ -14,13 +14,55 @@ export class InthenewsComponent implements OnInit {
   fullImagePath3 = '/assets/images/overland1.jpg';
   fullImagePath4 = '/assets/images/cambodiankids.JPG';
 
-  constructor() { }
+
+  constructor() {
+
+
+    window.onscroll = function () {
+      //console.log("help")
+
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+      } else {
+        document.getElementById("myBtn").style.display = "none";
+      }
+
+    };//this.scrollFunction() };
+
+  }
 
   ngOnInit() {
   }
 
   goTo(location: string): void {
     window.location.hash = location;
+  }
+
+  // When the user scrolls down 20px from the top of the document, show the button
+  //window.onscroll = function () { this.scrollFunction() };
+
+  // //function 
+  scrollFunction(): void {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById("myBtn").style.display = "block";
+    } else {
+      document.getElementById("myBtn").style.display = "none";
+    }
+  }
+
+  // When the user clicks on the button, scroll to the top of the document
+  //function 
+  topFunction(): void {
+
+    console.log('topFunction')
+
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+
+    //this.goTo('title');
+
+    window.scrollTo(0, 0)
+
   }
 
 
