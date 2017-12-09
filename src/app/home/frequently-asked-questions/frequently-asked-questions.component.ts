72d8c23a108a221ef32df2b3b704a8275a71da5a
Logan Born <lcborn4@gmail.com>
2017-12-09 18:18:07 -0500

    added the subtitle to the faq componetn

--- src/app/home/frequently-asked-questions/frequently-asked-questions.component.ts
+++ src/app/home/frequently-asked-questions/frequently-asked-questions.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FrequentlyAskedQuestionsComponent implements OnInit {
 
-  title = "Frequently Asked Questions"
+  title = "THFF FAQ"
+  subTitle = "(Frequently Asked Questions)"
 
   constructor() { }
 
