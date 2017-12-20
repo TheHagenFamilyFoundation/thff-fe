2ff82bbd090db473a13e67e7539eddf97ad606bd
Logan Born <lcborn4@gmail.com>
2017-12-20 17:39:17 -0500

    added the api service and the http client to the grant service

--- src/app/services/grant.service.ts
+++ src/app/services/grant.service.ts
@@ -1,15 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/observable';
+import { Grant } from '../grant'
 
 @Injectable()
 export class GrantService {
 
-  constructor() { }
+  //API_URL = 'https://hagenfoundationbackend.herokuapp.com'
+  API_URL = 'http://localhost:1337';
 
-  getGrants(): any {
+  grants = Grant[];
 
-    let grants = [];
+  constructor(private http: HttpClient) { }
 
-    return grants;
-  }
+  // getGrants(): Observable<grant[]> {
+
+  //   console.log("getGrants");
+
+  //   let urlString = this.API_URL + '/grant'
+
+  //   return this.http
+  //     .get(urlString)
+  //     .map(response => {
+  //       const grants = response.json();
+  //       return grants.map((grant) => new Grant(grant));
+  //     });
+
+  //   console.log("After request");
+
+
+  //   //return this.grants;
+  // }
 
 }
