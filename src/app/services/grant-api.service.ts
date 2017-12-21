0dc475db252410d831503e93f819d3880b9ee0fe
Logan Born <lcborn4@gmail.com>
2017-12-21 15:53:59 -0500

    commented out map and throw for the rxjs

--- src/app/services/grant-api.service.ts
+++ src/app/services/grant-api.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+//import { HttpClient } from '@angular/common/http';
 import { Grant } from '../grant'
 import { Observable } from 'rxjs/Observable'
-import 'rxjs/add/operator/map'
-import 'rxjs/add/operator/catch'
-import 'rxjs/add/operator/throw'
-
+// import 'rxjs/add/operator/map'
+ import 'rxjs/add/operator/catch'
+// import 'rxjs/add/operator/throw'
 
 @Injectable()
 export class GrantApiService {
