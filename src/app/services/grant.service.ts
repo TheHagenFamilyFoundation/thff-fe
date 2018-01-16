161b3b2c06fc67a45126292796b71b8c2ad4deb2
Logan Born <lcborn4@gmail.com>
2018-01-15 22:15:05 -0500

    added the grant api service to the grant service

--- src/app/services/grant.service.ts
+++ src/app/services/grant.service.ts
@@ -1,8 +1,49 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/observable';
+import { Grant } from '../grant'
+import { GrantApiService } from './grant-api.service'
 
 @Injectable()
 export class GrantService {
 
-  constructor() { }
+  constructor(
+    private grantapi: GrantApiService
+  ) {
+  }
 
+  // // Simulate POST /grant
+  // addTodo(grant: Grant): Observable<Grant> {
+  //   return this.grantapi.createGrant(grant);
+  // }
+
+  // // Simulate DELETE /grant/:id
+  // deleteGrantById(grantId: number): Observable<Grant> {
+  //   return this.grantapi.deleteGrantById(grantId);
+  // }
+
+  // // Simulate PUT /grant/:id
+  // updateGrant(grant: Grant): Observable<Grant> {
+  //   return this.grantapi.updateGrant(grant);
+  // }
+
+  // Simulate GET /grant
+  getAllGrants(): Observable<Grant[]> {
+    return this.grantapi.getAllGrants();
+  }
+
+  getGrantsByYear(year: number): Observable<Grant[]> {
+    return this.grantapi.getGrantsByYear(year);
+  }
+
+  // // Simulate GET /grant/:id
+  // getGrantById(grantId: number): Observable<Grant> {
+  //   return this.grantapi.getGrantById(grantId);
+  // }
+
+  // // Toggle complete
+  // toggleGrantComplete(grant: Grant) {
+  //   grant.complete = !grant.complete;
+  //   return this.grantapi.updateGrant(grant);
+  // }
 }
