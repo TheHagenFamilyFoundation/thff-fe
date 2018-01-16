05a02c29dbbbec87d75cd0d32311b17db0c25068
Logan Born <lcborn4@gmail.com>
2018-01-15 23:45:08 -0500

    changed the snack bar to matsnackbar

--- src/app/_guards/auth.guard.ts
+++ src/app/_guards/auth.guard.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
 import { Router, CanActivate } from '@angular/router';
-import { MdSnackBar } from '@angular/material';
+import { MatSnackBar } from '@angular/material';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-    constructor(private router: Router, public snackBar: MdSnackBar) {
+    constructor(private router: Router, public snackBar: MatSnackBar) {
 
         console.log("help - auth guard");
 
