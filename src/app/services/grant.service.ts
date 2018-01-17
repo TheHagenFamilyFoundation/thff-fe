8a201f525281962e1edbbd37de7cac8a83b01b1f
Logan Born <lcborn4@gmail.com>
2018-01-16 21:01:28 -0500

    capitalized the O in the Observable, in grant.service.ts

--- src/app/services/grant.service.ts
+++ src/app/services/grant.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/observable';
+import { Observable } from 'rxjs/Observable';
 import { Grant } from '../grant'
 import { GrantApiService } from './grant-api.service'
 
