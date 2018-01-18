dc890588002df7b4a7f311113fc149dee8856953
Logan Born <lcborn4@gmail.com>
2018-01-17 19:50:57 -0500

    created the valid-email service

---
+++ src/app/valid-email.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { ValidEmailService } from './valid-email.service';
+
+describe('ValidEmailService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [ValidEmailService]
+    });
+  });
+
+  it('should be created', inject([ValidEmailService], (service: ValidEmailService) => {
+    expect(service).toBeTruthy();
+  }));
+});
