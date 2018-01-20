623535c338974f67e1d6ba8a35cbf6a713ba7199
Logan Born <lcborn4@gmail.com>
2018-01-20 15:18:51 -0500

    added the environment file and used the environment API URL to the valid-email service

--- src/app/services/valid-email.service.ts
+++ src/app/services/valid-email.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable'
+import { Observable } from 'rxjs/Observable';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class ValidEmailService {
 
+  API_URL = environment.API_URL;
   //API_URL = 'https://hagenfoundationbackend.herokuapp.com'
-  API_URL = 'http://localhost:1337';
+  //API_URL = 'http://localhost:1337';
 
   constructor(private http: HttpClient) { }
 
