d769fa48cd9647c63abe5dd0dead705fce4c963b
Logan Born <lcborn4@gmail.com>
2018-01-26 15:41:13 -0500

    updated the email services filepath in the email component

--- src/app/test/email/email.component.ts
+++ src/app/test/email/email.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { EmailService } from '../../email.service';
+import { EmailService } from '../../services/email.service';
 import { Router } from '@angular/router';
 
 @Component({
