4574509bc9b52b5cb336880fd99eb67f7c4c9961
Logan Born <lcborn4@gmail.com>
2018-01-27 18:16:03 -0500

    added the email service to the reset-password component

--- src/app/login/reset-password-or-forgot-username/reset-password/reset-password.component.ts
+++ src/app/login/reset-password-or-forgot-username/reset-password/reset-password.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material';
+import { ValidEmailService } from '../../../services/valid-email.service';
+import { ValidUserNameService } from '../../../services/valid-username.service';
+import { environment } from '../../../../environments/environment';
+import { EmailService } from '../../../services/email.service';
 
 @Component({
   selector: 'app-reset-password',
@@ -11,10 +15,26 @@ export class ResetPasswordComponent implements OnInit {
 
   title = "Reset Password"
 
-  username: any;
+  API_URL = environment.API_URL;
+
+  userName: any;
   email: any;
 
-  constructor(private router: Router, public snackBar: MatSnackBar) { }
+  ValidUserName: any;
+  ValidEmail: any;
+
+  ShowUserNameError = false;
+  ShowEmailError = false;
+
+  CanResetPassword = false; //initialize to false
+
+  constructor(
+    private router: Router,
+    public snackBar: MatSnackBar,
+    private validEmailService: ValidEmailService,
+    private validUserNameService: ValidUserNameService,
+    private _emailService: EmailService
+  ) { }
 
   ngOnInit() {
   }
@@ -23,22 +43,125 @@ export class ResetPasswordComponent implements OnInit {
 
     console.log("You clicked on the Reset Password")
 
-    let snackBarRef = this.snackBar.open('Password has been reset. Please check your email.', 'OK', {
+    let snackBarRef = this.snackBar.open('Please check your email for reset instructions.', 'OK', {
       duration: 3000
     });
 
-    //check if username and email are valid then sends email
-    this.checkValidUserNameEmail();
+    //create random function that creates a password
+
+    let message = "Your password is reset"
+
+    this._emailService.sendEmail({
+      from: 'Mailgun Sandbox <postmaster@sandboxXXXXXXXXXXXXXXXXXXXXX.mailgun.org>',
+      to: this.email,
+      name: name,
+      text: message,
+    })
+      .subscribe(
+      () => { },
+      err => console.log(err)
+      );
 
     this.router.navigate(['/login']);
 
   }
 
-  checkValidUserNameEmail(): void {
+  UsernameChange(event) {
+    console.log("UsernameChange");
 
-    console.log("Checking if Username and Email are valid");
+    console.log(event);
 
+    if (this.userName != "") {
 
+      this.ValidUserCheck();
+
+    }
+    else {
+
+      this.ValidUserName = false;
+
+    }
+
+    this.VerifyInput();
+  }
+
+  EmailChange(event) {
+    console.log("EmailChange");
+
+    console.log(event);
+
+    if (this.email != "") {
+      //this.ValidEmail = true;
+      this.ValidEmailCheck();
+    }
+    else {
+      this.ValidEmail = false;
+    }
+
+    this.VerifyInput();
+  }
+
+  ValidUserCheck(): void {
+
+    this.ValidUserName = this.validUserNameService.checkValidUserName(this.userName)
+      .subscribe(
+      (data) => {
+        //debug
+        console.log(data);
+        console.log("data");
+
+        if (data.userfound) {
+          console.log("user found");
+
+          this.ShowUserNameError = false;
+        }
+        else {
+          console.log("user not found");
+
+          this.ShowUserNameError = true;
+
+        }
+
+        this.VerifyInput();
+
+      });
+
+  }
+
+  ValidEmailCheck(): void {
+
+    this.ValidEmail = this.validEmailService.checkValidEmail(this.email)
+      .subscribe(
+      (data) => {
+        //debug
+        console.log(data);
+        console.log("data");
+
+        if (data.emailfound) {
+          console.log("email found");
+
+          this.ShowEmailError = false;
+        }
+        else {
+          console.log("email not found");
+
+          this.ShowEmailError = true;
+
+        }
+
+        this.VerifyInput();
+
+      });
+
+  }
+
+  VerifyInput(): void {
+    if (this.ValidUserName && this.ValidEmail) {
+      this.CanResetPassword = true;
+    }
+    else {
+      this.CanResetPassword = false;
+    }
   }
 
 
