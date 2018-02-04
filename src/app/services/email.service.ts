e1f9ba01a8b4d5bc6773355f013760674bff684b
Logan Born <lcborn4@gmail.com>
2018-02-03 19:19:20 -0500

    created a new send email function: sendResetEmail

--- src/app/services/email.service.ts
+++ src/app/services/email.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class EmailService {
 
+  API_URL = environment.API_URL;
+
   constructor(private http: HttpClient) { }
 
+  //debug
   sendEmail(data) {
-    return this.http.post('http://localhost:1337/email/', data)
+    return this.http.post(this.API_URL + '/email/', data)
     //.map(res => res.json())
     //.catch(this._errorHandler);
   }
+
+  sendResetEmail(data) {
+    return this.http.post(this.API_URL + '/sendResetEmail', data)
+  }
+
 
   private _errorHandler(error: Response) {
     console.error(error);
