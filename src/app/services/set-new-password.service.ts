bf7134884bf49b3c1bbf423c80d66500ab8ba99f
Logan Born <lcborn4@gmail.com>
2018-02-24 14:05:00 -0500

    added the httpclient, environment variables, and created the setNewPassword function

--- src/app/services/set-new-password.service.ts
+++ src/app/services/set-new-password.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class SetNewPasswordService {
 
-  constructor() { }
+  API_URL = environment.API_URL;
+
+  constructor(private http: HttpClient) { }
+
+  setNewPassword(data): Observable<any> {
+    //data is the user email
+    return this.http.post(this.API_URL + '/setNewPassword', data)
+  }
 
 }
