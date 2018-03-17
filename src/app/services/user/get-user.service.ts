1186bed9875efce62fbd3a51c1637c9a0b62b24c
Logan Born <lcborn4@gmail.com>
2018-03-17 15:12:26 -0400

    getUser service now

---
+++ src/app/services/user/get-user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { environment } from '../../../environments/environment';
+
+@Injectable()
+export class GetUserService {
+
+  API_URL = environment.API_URL;
+
+  constructor(private http: HttpClient) { }
+
+  //maybe make a getUser by ID
+  getUserbyUsername(username: string): Observable<any> {
+
+    let urlString = this.API_URL + "/user?username=" + username;
+
+    return this.http.get(urlString);
+  }
+
+}
