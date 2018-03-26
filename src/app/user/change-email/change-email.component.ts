a24704d33dc9c3ddc9fec4c06be860e9a5d9d2ec
Logan Born <lcborn4@gmail.com>
2018-03-25 21:35:39 -0400

    added the change email functionality, backend doesn't work

--- src/app/user/change-email/change-email.component.ts
+++ src/app/user/change-email/change-email.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+
+//debounce
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/distinctUntilChanged';
+import 'rxjs/add/operator/switchMap';
 
 @Component({
   selector: 'app-change-email',
@@ -7,9 +13,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChangeEmailComponent implements OnInit {
 
-  constructor() { }
+  newEmail$ = new Subject<string>();
+
+  email: any;
+  newEmail: any;
+
+  @Input()
+  user: any;
+
+  CanChangeEmail = false;
+
+  constructor() {
+
+    this.newEmail$
+      .debounceTime(400)
+      .distinctUntilChanged()
+      .subscribe(term => {
+
+        this.newEmail = term;
+        this.newEmailChange()
+      });
+
+  }
 
   ngOnInit() {
+
+    console.log(this.user.email);
+
+    this.email = this.user.email;
+
+  }
+
+  changeEmail(): void {
+    console.log("changeEmail Pressed");
+
+    
+
+  }
+
+  newEmailChange(): void {
+
+    console.log("newEmailChange");
+
+    //add in logic for valid email
+    
+
+    if (this.newEmail != "") {
+      this.CanChangeEmail = true;
+    }
+    else {
+      this.CanChangeEmail = false;
+    }
   }
 
 }
