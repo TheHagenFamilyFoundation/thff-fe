171dd2e330af3e8313ee95b7319c5ae8e427426b
Logan Born <lcborn4@gmail.com>
2018-03-27 22:18:22 -0400

    created user-grant-requests

---
+++ src/app/user/user-grant-requests/user-grant-requests.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-grant-requests',
+  templateUrl: './user-grant-requests.component.html',
+  styleUrls: ['./user-grant-requests.component.css']
+})
+export class UserGrantRequestsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
