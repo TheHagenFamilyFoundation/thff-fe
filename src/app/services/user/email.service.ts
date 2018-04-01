12da77ca2a2ca5be8c96ea6f08094013dcec6281
Logan Born <lcborn4@gmail.com>
2018-04-01 15:37:18 -0400

    created post routes

--- src/app/services/user/email.service.ts
+++ src/app/services/user/email.service.ts
@@ -18,19 +18,19 @@ export class EmailService {
   }
 
   sendResetEmail(data) {
-    return this.http.put(this.API_URL + '/sendResetEmail', data)
+    return this.http.post(this.API_URL + '/sendResetEmail', data)
   }
 
   sendRegisterUserEmail(data) {
-    return this.http.put(this.API_URL + '/sendRegisterUserEmail', data)
+    return this.http.post(this.API_URL + '/sendRegisterUserEmail', data)
   }
 
   sendResetEmailConfirmation(data) {
-    return this.http.put(this.API_URL + '/sendResetEmailConfirmation', data)
+    return this.http.post(this.API_URL + '/sendResetEmailConfirmation', data)
   }
 
   sendUserNameEmail(data) {
-    return this.http.put(this.API_URL + '/sendUserNameEmail', data)
+    return this.http.post(this.API_URL + '/sendUserNameEmail', data)
   }
 
   private _errorHandler(error: Response) {
