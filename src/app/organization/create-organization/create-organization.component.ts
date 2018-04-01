293b3189e128d86f88298c7b6d86746cd685ed62
Logan Born <lcborn4@gmail.com>
2018-04-01 19:55:42 -0400

    created the create-organization page

---
+++ src/app/organization/create-organization/create-organization.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-organization',
+  templateUrl: './create-organization.component.html',
+  styleUrls: ['./create-organization.component.css']
+})
+export class CreateOrganizationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
