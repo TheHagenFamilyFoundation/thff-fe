187d6d147d7276718be735e8fbff4983ff4d77f0
Logan Born <lcborn4@gmail.com>
2018-05-29 22:06:08 -0400

    added activated router to access the loiID and retrieve the LOI

--- src/app/letter-of-intent/letter-of-intent.component.ts
+++ src/app/letter-of-intent/letter-of-intent.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+
+import { GetLoiService } from '../services/loi/get-loi.service';
 
 @Component({
   selector: 'app-letter-of-intent',
@@ -7,9 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LetterOfIntentComponent implements OnInit {
 
-  constructor() { }
+  loiID: any;
+
+  loi: any; //the loi object
+
+  //check basic row height
+  basicRowHeight = 400;
+
+  constructor(
+    private route: ActivatedRoute,
+    public getLoiService: GetLoiService,
+  ) {
+    this.route.params.subscribe(params => {
+      console.log(params);
+      this.loiID = params.id;
+    });
+  }
 
   ngOnInit() {
+
+    console.log('loiID', this.loiID)
+
+    this.getLOI(this.loiID);
+
+
+  }
+
+  getLOI(loiID) {
+
+    console.log('check loi');
+
+    //query database for that loi
+
+    this.getLoiService.getLOIbyID(loiID)
+      .subscribe(
+        (loi) => {
+
+          console.log('loi', loi);
+
+          this.loi = loi[0];
+
+        })
+
   }
 
 }
