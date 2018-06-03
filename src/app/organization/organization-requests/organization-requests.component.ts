7bf03bda2cd615fc71e429757f8df0d87adf2318
Logan Born <lcborn4@gmail.com>
2018-06-03 17:08:24 -0400

    added table and loi list

--- src/app/organization/organization-requests/organization-requests.component.ts
+++ src/app/organization/organization-requests/organization-requests.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, ViewChild, OnInit, Input } from '@angular/core';
+import { MatPaginator, MatSort, MatTableDataSource } from '@angular/material';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-organization-requests',
@@ -10,9 +12,44 @@ export class OrganizationRequestsComponent implements OnInit {
   @Input()
   org: any;
 
+  lois: any;
+
+  HasLOIs = false;
+
+  NoLOIs = 'No LOIs'
+
+  // displayedColumns = ['id', 'name', 'progress', 'color'];
+  displayedColumns = ['name'];
+  dataSource: any;//MatTableDataSource<OrganizationData>;
+
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
+
   constructor() { }
 
   ngOnInit() {
+
+    console.log('this.org', this.org)
+    console.log('this.org.users', this.org.users)
+    this.lois = this.org.lois;
+    this.dataSource = this.lois;
+
+    if (this.lois.length > 0) {
+
+      this.HasLOIs = true;
+    }
+    else
+    {
+      this.HasLOIs = false;
+    }
+
+  }
+
+  onRowClicked(row) {
+    console.log('Row clicked: ', row);
+
+    //this.openSelectedOrgDialog(row); //pass in the org from row object
+
   }
 
 }
