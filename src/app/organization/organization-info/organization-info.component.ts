65c6a8b4b742cca0a18cf0597d92c46a384fad0b
Logan Born <lcborn4@gmail.com>
2018-06-06 23:00:48 -0400

    added editing

--- src/app/organization/organization-info/organization-info.component.ts
+++ src/app/organization/organization-info/organization-info.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-organization-info',
@@ -7,9 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrganizationInfoComponent implements OnInit {
 
+  @Input()
+  org: any;
+
+  editing = false;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  edit() {
+    console.log('edit pressed')
+
+    this.editing = true;
+
+  }
+
+  save() {
+    console.log('save pressed')
+
+    this.editing = false;
   }
 
 }
