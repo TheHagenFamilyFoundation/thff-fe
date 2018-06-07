260f4c074b5f07053252e4fb337fe7e66b4cdaa7
Logan Born <lcborn4@gmail.com>
2018-06-06 22:00:51 -0400

    organization info component

---
+++ src/app/organization/organization-info/organization-info.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OrganizationInfoComponent } from './organization-info.component';
+
+describe('OrganizationInfoComponent', () => {
+  let component: OrganizationInfoComponent;
+  let fixture: ComponentFixture<OrganizationInfoComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ OrganizationInfoComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OrganizationInfoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/organization/organization-info/organization-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-organization-info',
+  templateUrl: './organization-info.component.html',
+  styleUrls: ['./organization-info.component.css']
+})
+export class OrganizationInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
