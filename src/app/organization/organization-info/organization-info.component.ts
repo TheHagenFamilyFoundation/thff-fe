9641e2a8375ccefcd4d7f3d439c8bf9a9fcf7faf
Logan Born <lcborn4@gmail.com>
2018-06-07 22:37:16 -0400

    added subject to organization info

--- src/app/organization/organization-info/organization-info.component.ts
+++ src/app/organization/organization-info/organization-info.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+//debounce
+import { Subject } from 'rxjs';
+
+import { map, takeUntil, tap, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+
 @Component({
   selector: 'app-organization-info',
   templateUrl: './organization-info.component.html',
@@ -10,7 +15,35 @@ export class OrganizationInfoComponent implements OnInit {
   @Input()
   org: any;
 
+  legalName$ = new Subject<string>();
+  yearFounded$ = new Subject<string>();
+  currentOperatingBudget$ = new Subject<string>();
+  director$ = new Subject<string>();
+  legalNphoneame$ = new Subject<string>();
+  phone$ = new Subject<string>();
+  contactPerson$ = new Subject<string>();
+  email$ = new Subject<string>();
+  address$ = new Subject<string>();
+  city$ = new Subject<string>();
+  state$ = new Subject<string>();
+  zip$ = new Subject<string>();
+  fax$ = new Subject<string>();
+
+
   editing = false;
+
+  legalName: string; //  -Legal Name of Organization Applying: 
+  yearFounded: number; // -Year Founded 
+  currentOperatingBudget: number; // -Current Operating Budget 
+  director: string; // -Executive Director 
+  phone: string; // -Phone Number 
+  contactPerson: string; //-Contact person/title/phone number 
+  email: string; // -Email Address 
+  address: string; //-Address (principal/administrative office) 
+  city: string; // -City 
+  state: string;// -State 
+  zip: number;//-Zip 
+  fax: number; //-Fax Number
 
   constructor() { }
 
