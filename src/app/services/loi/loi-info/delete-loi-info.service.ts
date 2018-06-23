a906a4a495b273f1fc39e2e34ff96a379416c299
Logan Born <lcborn4@gmail.com>
2018-06-23 17:10:57 -0400

    created deleteLoiInfobyLoiInfoID

--- src/app/services/loi/loi-info/delete-loi-info.service.ts
+++ src/app/services/loi/loi-info/delete-loi-info.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs'
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeleteLoiInfoService {
 
-  constructor() { }
+  API_URL = environment.API_URL;
+
+  results;
+  body;
+
+  constructor(private http: HttpClient) { }
+
+  deleteLoiInfobyLoiInfoID(loiInfoID: string): Observable<any> {
+
+    console.log('deleteLoiInfobyLoiID')
+
+    let urlString = this.API_URL + "/loiInfo/" + loiInfoID;
+
+    return this.http.delete(urlString);
+
+  }
+
 }
