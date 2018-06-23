8fb7c7d81225dc530f279e23999f3520f8ae5da6
Logan Born <lcborn4@gmail.com>
2018-06-23 17:18:24 -0400

    create letter of intent info

---
+++ src/app/letter-of-intent/letter-of-intent-info/letter-of-intent-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-letter-of-intent-info',
+  templateUrl: './letter-of-intent-info.component.html',
+  styleUrls: ['./letter-of-intent-info.component.css']
+})
+export class LetterOfIntentInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
