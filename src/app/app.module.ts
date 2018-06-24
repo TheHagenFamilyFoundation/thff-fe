50a12aa8e59a3a34f309bc17e7edcc7428c05ab6
Logan Born <lcborn4@gmail.com>
2018-06-23 20:10:19 -0400

    reactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { HttpClientModule, HttpClientXsrfModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 import { AppComponent } from './app.component';
 import 'hammerjs';
 
@@ -192,7 +192,7 @@ import { EmailComponent } from './test/email/email.component';
         }),
 
         FormsModule, BrowserAnimationsModule,
-        AppRoutingModule,
+        AppRoutingModule, ReactiveFormsModule, 
 
         FlexLayoutModule,
 
