31667a055d7201cd8f88f356d6cc3bcac09bbab5
Logan Born <lcborn4@gmail.com>
2018-06-30 19:21:37 -0400

    create organization header - the modal from the header

---
+++ src/app/organization/create-organization-header/create-organization-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-organization-header',
+  templateUrl: './create-organization-header.component.html',
+  styleUrls: ['./create-organization-header.component.css']
+})
+export class CreateOrganizationHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
