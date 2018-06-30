1555b0b69fd7b40912ea389696eaf4c96eae79b4
Logan Born <lcborn4@gmail.com>
2018-06-30 19:21:54 -0400

    after the user fills out the org name and the description and then hits next

---
+++ src/app/organization/create-organization-full/create-organization-full.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-organization-full',
+  templateUrl: './create-organization-full.component.html',
+  styleUrls: ['./create-organization-full.component.css']
+})
+export class CreateOrganizationFullComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
