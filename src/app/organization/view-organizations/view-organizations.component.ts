06877a11e13f5eafd408701167b469b961219ed2
Logan Born <lcborn4@gmail.com>
2018-07-01 17:38:29 -0400

    created view-organizations

---
+++ src/app/organization/view-organizations/view-organizations.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-organizations',
+  templateUrl: './view-organizations.component.html',
+  styleUrls: ['./view-organizations.component.css']
+})
+export class ViewOrganizationsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
