2f64a48523ea3894a710e6b2676a05226944e4de
Logan Born <lcborn4@gmail.com>
2018-07-02 18:29:18 -0400

    create loi full

---
+++ src/app/letter-of-intent/create-letter-of-intent-full/create-letter-of-intent-full.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-letter-of-intent-full',
+  templateUrl: './create-letter-of-intent-full.component.html',
+  styleUrls: ['./create-letter-of-intent-full.component.css']
+})
+export class CreateLetterOfIntentFullComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
