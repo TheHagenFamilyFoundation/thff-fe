7d362edd5f12086dd4d4c0181a8059df3277a88e
Logan Born <lcborn4@gmail.com>
2018-07-04 16:30:13 -0400

    upload the 501c3 function

--- src/app/organization/organization.component.ts
+++ src/app/organization/organization.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from "@angular/router";
 
 import { GetOrganizationService } from '../services/organization/get-organization.service';
 
+import { Upload501c3Service } from '../services/organization/upload-501c3.service';
+
 @Component({
   selector: 'app-organization',
   templateUrl: './organization.component.html',
@@ -20,6 +22,7 @@ export class OrganizationComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     public getOrgService: GetOrganizationService,
+    private upload501c3Service: Upload501c3Service,
   ) {
     this.route.params.subscribe(params => {
       console.log(params);
@@ -51,6 +54,25 @@ export class OrganizationComponent implements OnInit {
 
         })
 
+  }
+
+  fileChange(event) {
+
+    console.log('fileChange', event)
+
+    let fileList: FileList = event.target.files;
+    if (fileList.length > 0) {
+      let file: File = fileList[0];
+      // let formData: FormData = new FormData();
+      // formData.append('uploadFile', file, file.name);
+
+      this.upload501c3Service.upload501c3(file)
+        .subscribe(
+          () => { '501c3 uploaded' },
+          err => console.log(err)
+        );
+
+    }
   }
 
 }
