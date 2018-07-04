f4886e01194369966c9842bafa7db7d2fa24b3de
Logan Born <lcborn4@gmail.com>
2018-07-04 17:41:30 -0400

    added the get and delete 501c3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -66,6 +66,8 @@ import { GetOrganizationInfoService } from './services/organization/organization
 import { DeleteOrganizationInfoService } from './services/organization/organization-info/delete-organization-info.service';
 
 import { Upload501c3Service } from './services/organization/501c3/upload-501c3.service';
+import { Get501c3Service } from './services/organization/501c3/get-501c3.service';
+import { Delete501c3Service } from './services/organization/501c3/delete-501c3.service'; //for updating the 501c3 if there are any changes
 
 import { AddUserService } from './services/organization/add-user.service';
 
@@ -231,7 +233,7 @@ import { EmailComponent } from './test/email/email.component';
         InOrgService,
         GetOrganizationService, CreateOrganizationService, AddUserService,
         CreateOrganizationInfoService, GetOrganizationInfoService, DeleteOrganizationInfoService,
-        Upload501c3Service,
+        Upload501c3Service, Get501c3Service, Delete501c3Service,
         GetLoiService, CreateLoiService,
         CreateLoiInfoService, GetLoiInfoService, DeleteLoiInfoService,
     ],
