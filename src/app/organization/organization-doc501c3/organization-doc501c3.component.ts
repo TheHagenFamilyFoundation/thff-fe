b14a157fd817ef80839690721b5636531081d088
Logan Born <lcborn4@gmail.com>
2018-07-08 15:48:05 -0400

    created the organization-doc501c3 component

---
+++ src/app/organization/organization-doc501c3/organization-doc501c3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-organization-doc501c3',
+  templateUrl: './organization-doc501c3.component.html',
+  styleUrls: ['./organization-doc501c3.component.css']
+})
+export class OrganizationDoc501c3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
