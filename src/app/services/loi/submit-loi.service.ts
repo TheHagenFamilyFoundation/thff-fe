a3624d886cf55d92fb014e299592bc109e095af3
Logan Born <lcborn4@gmail.com>
2018-07-15 13:11:07 -0400

    submit loi function - get

--- src/app/services/loi/submit-loi.service.ts
+++ src/app/services/loi/submit-loi.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubmitLoiService {
 
-  constructor() { }
+  API_URL = environment.API_URL;
+
+  constructor(private http: HttpClient) { }
+
+  submitLOI(loiID: string): Observable<any> {
+    return this.http.get(this.API_URL + '/submitLOI' + loiID)
+  }
+
 }
