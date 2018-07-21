b40a9e76336d687d92480bcb14b31bcfc47c762e
Logan Born <lcborn4@gmail.com>
2018-07-21 16:28:55 -0400

    selected org director created

---
+++ src/app/director/director-organizations/director-selected-organization/director-selected-organization.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-director-selected-organization',
+  templateUrl: './director-selected-organization.component.html',
+  styleUrls: ['./director-selected-organization.component.css']
+})
+export class DirectorSelectedOrganizationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
