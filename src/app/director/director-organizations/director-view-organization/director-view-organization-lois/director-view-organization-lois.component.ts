4af5666ee3b0ab8554d0c24c539d71d37bee7ccb
Logan Born <lcborn4@gmail.com>
2018-07-21 18:17:42 -0400

    view org lois from director perspective

---
+++ src/app/director/director-organizations/director-view-organization/director-view-organization-lois/director-view-organization-lois.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-director-view-organization-lois',
+  templateUrl: './director-view-organization-lois.component.html',
+  styleUrls: ['./director-view-organization-lois.component.css']
+})
+export class DirectorViewOrganizationLoisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
