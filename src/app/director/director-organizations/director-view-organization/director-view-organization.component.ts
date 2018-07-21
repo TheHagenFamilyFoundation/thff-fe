193ccdb162bd53d197c062fe2325574cfee45b2a
Logan Born <lcborn4@gmail.com>
2018-07-21 17:33:10 -0400

    view the org from director perspective

--- src/app/director/director-organizations/director-view-organization/director-view-organization.component.ts
+++ src/app/director/director-organizations/director-view-organization/director-view-organization.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";
+
+import { GetOrganizationService } from '../../../services/organization/get-organization.service';
 
 @Component({
   selector: 'app-director-view-organization',
@@ -7,9 +10,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectorViewOrganizationComponent implements OnInit {
 
-  constructor() { }
+  org: any;
+
+  orgID: string;
+
+  organizationID: string;
+
+  constructor(private route: ActivatedRoute,
+    private router: Router,
+    public getOrgService: GetOrganizationService,
+  ) {
+
+    this.route.params.subscribe(params => {
+      console.log(params);
+      this.orgID = params.id;
+    });
+
+  }
 
   ngOnInit() {
+
+    console.log('orgID', this.orgID)
+
+    this.getOrganization(this.orgID);
+
+  }
+
+  getOrganization(orgID) {
+
+    console.log('check organizations');
+
+    //query database for that organization
+    this.getOrgService.getOrgbyID(orgID)
+      .subscribe(
+        (org) => {
+
+          console.log('org', org);
+
+          this.org = org[0];
+
+          this.organizationID = this.org.id;
+
+        })
+
   }
 
 }
