ad6b14e21580d3d78f2af1b478a149aab21aa35a
Logan Born <lcborn4@gmail.com>
2018-07-21 17:32:59 -0400

    selected org from director

--- src/app/director/director-organizations/director-selected-organization/director-selected-organization.component.ts
+++ src/app/director/director-organizations/director-selected-organization/director-selected-organization.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-director-selected-organization',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectorSelectedOrganizationComponent implements OnInit {
 
-  constructor() { }
+  orgLink = '/director-organization/'
+  link: string;
+
+  orgID: any;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    // will log the entire data object
+    console.log('this.data', this.data)
+
+    this.orgID = this.data.orgID;
+
+    this.link = this.orgLink + this.data.orgID;
+
+    console.log('this is the link', this.link);
+
+  }
+
+  cancel() {
+
+    console.log('cancel pressed');
+
   }
 
 }
