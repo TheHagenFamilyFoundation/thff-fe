6593dc2e2135c287087068db875cf83db6b640df
Logan Born <lcborn4@gmail.com>
2018-07-21 18:17:02 -0400

    doc501c3 from director perspective

---
+++ src/app/director/director-organizations/director-view-organization/director-view-organization-doc501c3/director-view-organization-doc501c3.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-director-view-organization-doc501c3',
+  templateUrl: './director-view-organization-doc501c3.component.html',
+  styleUrls: ['./director-view-organization-doc501c3.component.css']
+})
+export class DirectorViewOrganizationDoc501c3Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
