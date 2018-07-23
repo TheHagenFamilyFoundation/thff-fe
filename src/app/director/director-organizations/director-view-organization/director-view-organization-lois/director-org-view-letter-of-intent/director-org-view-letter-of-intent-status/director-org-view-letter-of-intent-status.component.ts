330b510ca725ba811ea6db527ea2b43da97479ea
Logan Born <lcborn4@gmail.com>
2018-07-22 22:38:17 -0400

    view loi from director

---
+++ src/app/director/director-organizations/director-view-organization/director-view-organization-lois/director-org-view-letter-of-intent/director-org-view-letter-of-intent-status/director-org-view-letter-of-intent-status.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-director-org-view-letter-of-intent-status',
+  templateUrl: './director-org-view-letter-of-intent-status.component.html',
+  styleUrls: ['./director-org-view-letter-of-intent-status.component.css']
+})
+export class DirectorOrgViewLetterOfIntentStatusComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
