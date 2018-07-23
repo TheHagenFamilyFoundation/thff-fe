3527a9341416e8e3622cbba87a99ab4ccc92bb69
Logan Born <lcborn4@gmail.com>
2018-07-22 21:46:49 -0400

    selected letter of intent

---
+++ src/app/director/director-organizations/director-view-organization/director-view-organization-lois/director-org-selected-letter-of-intent/director-org-selected-letter-of-intent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-director-org-selected-letter-of-intent',
+  templateUrl: './director-org-selected-letter-of-intent.component.html',
+  styleUrls: ['./director-org-selected-letter-of-intent.component.css']
+})
+export class DirectorOrgSelectedLetterOfIntentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
