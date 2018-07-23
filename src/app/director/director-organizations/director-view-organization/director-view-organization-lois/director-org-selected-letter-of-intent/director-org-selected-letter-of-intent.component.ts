af6647ec9f035f7ebd4f1924097c4fef8c7c328e
Logan Born <lcborn4@gmail.com>
2018-07-22 22:20:28 -0400

    selected loi modal

--- src/app/director/director-organizations/director-view-organization/director-view-organization-lois/director-org-selected-letter-of-intent/director-org-selected-letter-of-intent.component.ts
+++ src/app/director/director-organizations/director-view-organization/director-view-organization-lois/director-org-selected-letter-of-intent/director-org-selected-letter-of-intent.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-director-org-selected-letter-of-intent',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectorOrgSelectedLetterOfIntentComponent implements OnInit {
 
-  constructor() { }
+  loiLink = '/director-loi/'
+  link: string;
+
+  loiID: any;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    // will log the entire data object
+    console.log('this.data', this.data)
+
+    this.loiID = this.data.loiID;
+
+    this.link = this.loiLink + this.data.loiID;
+
+    console.log('this is the link', this.link);
+
+  }
+
+  cancel() {
+
+    console.log('cancel pressed');
+
   }
 
 }
