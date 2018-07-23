45bdb599fd6d04b05a23e35350056d0d723e5cf4
Logan Born <lcborn4@gmail.com>
2018-07-22 21:54:13 -0400

    created director view loi

---
+++ src/app/director/director-organizations/director-view-organization/director-view-organization-lois/director-org-view-letter-of-intent/director-org-view-letter-of-intent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-director-org-view-letter-of-intent',
+  templateUrl: './director-org-view-letter-of-intent.component.html',
+  styleUrls: ['./director-org-view-letter-of-intent.component.css']
+})
+export class DirectorOrgViewLetterOfIntentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
