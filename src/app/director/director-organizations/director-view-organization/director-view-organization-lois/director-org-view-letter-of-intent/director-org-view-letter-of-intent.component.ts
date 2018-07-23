dc820514fc49b4a4d88d55f7f071309353bc96c5
Logan Born <lcborn4@gmail.com>
2018-07-22 22:20:34 -0400

    get the loi

--- src/app/director/director-organizations/director-view-organization/director-view-organization-lois/director-org-view-letter-of-intent/director-org-view-letter-of-intent.component.ts
+++ src/app/director/director-organizations/director-view-organization/director-view-organization-lois/director-org-view-letter-of-intent/director-org-view-letter-of-intent.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+
+import { GetLoiService } from '../../../../../services/loi/get-loi.service';
+import { LOIStatusService } from "../../../../../services/loi/loi-status.service";
 
 @Component({
   selector: 'app-director-org-view-letter-of-intent',
@@ -7,9 +11,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectorOrgViewLetterOfIntentComponent implements OnInit {
 
-  constructor() { }
+  loiID: any;
+
+  loi: any; //the loi object
+
+  organization: any; //organization object retrieved from the loi object
+  orgName: string;
+
+  status: number;
+
+  constructor(
+    private route: ActivatedRoute,
+    public getLoiService: GetLoiService,
+    private loiStatus: LOIStatusService
+  ) {
+
+    this.route.params.subscribe(params => {
+      console.log(params);
+      this.loiID = params.id;
+    });
+
+  }
 
   ngOnInit() {
+
+    console.log('loiID', this.loiID)
+
+    this.getLOI(this.loiID);
+
+  }
+
+  getLOI(loiID) {
+
+    console.log('check loi');
+
+    //query database for that loi
+
+    this.getLoiService.getLOIbyID(loiID)
+      .subscribe(
+        (loi) => {
+
+          console.log('loi', loi);
+
+          this.loi = loi[0];
+
+          this.organization = loi[0].organization;
+
+          console.log('organization', this.organization);
+
+          this.orgName = this.organization.name;
+
+          this.setStatus();
+
+        })
+
+  }
+
+  setStatus() {
+
+    this.status = this.loi.status;
+
   }
 
 }
