fcc1c9027fd284704bb8d9a835bfa96d00e4d2dc
Logan Born <lcborn4@gmail.com>
2018-07-24 22:13:41 -0400

    created director selected loi - modal

---
+++ src/app/director/director-lois/director-selected-loi/director-selected-loi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-director-selected-loi',
+  templateUrl: './director-selected-loi.component.html',
+  styleUrls: ['./director-selected-loi.component.css']
+})
+export class DirectorSelectedLoiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
