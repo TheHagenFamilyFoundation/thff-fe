4b1db648ae7a7f5507596b8e3802059cfd4496fb
Logan Born <lcborn4@gmail.com>
2018-07-24 21:41:24 -0400

    director lois

---
+++ src/app/director/director-lois/director-lois.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-director-lois',
+  templateUrl: './director-lois.component.html',
+  styleUrls: ['./director-lois.component.css']
+})
+export class DirectorLoisComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
