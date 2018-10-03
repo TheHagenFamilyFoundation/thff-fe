f4ac75e5b14bdb6906f71957aeb7a69831c4dbb4
Logan Born <lcborn4@gmail.com>
2018-10-02 22:29:29 -0400

    backend route

--- server.js
+++ server.js
@@ -4,12 +4,18 @@ const app = express();
 const path = require('path');
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static( __dirname + '/dist/'));
+app.use(express.static(__dirname + '/dist/'));
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
-app.get('/*', function(req, res) {
+app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
+
+// Nice and done!
+app.get('/backend', (req, res) => {
+  res.json({ url: process.env.BE_API })
+});
+
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
