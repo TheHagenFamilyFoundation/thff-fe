877f59356be9138dfd93a54a0037f8f3d55b2525
Logan Born <lcborn4@gmail.com>
2018-10-13 18:15:10 -0400

    spacing, set session variable

--- server.js
+++ server.js
@@ -12,13 +12,11 @@ app.get('/backend', (req, res) => {
 });
 
 
-
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
-
 
 // Start the app by listening on the default
 // Heroku port
