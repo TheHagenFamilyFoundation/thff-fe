79208136d514ed7f927599459249ab7aabbbba3d
Logan Born <lcborn4@gmail.com>
2018-11-05 22:42:47 -0500

    full proposal submit check component

---
+++ src/app/full-proposal/full-proposal-submit-check/full-proposal-submit-check.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-full-proposal-submit-check',
+  templateUrl: './full-proposal-submit-check.component.html',
+  styleUrls: ['./full-proposal-submit-check.component.css']
+})
+export class FullProposalSubmitCheckComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
