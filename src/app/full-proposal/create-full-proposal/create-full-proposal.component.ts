e778383bf3d5cb2561825716a5c227915e0e8084
Logan Born <lcborn4@gmail.com>
2018-11-10 22:20:22 -0500

    full proposal modal created

--- src/app/full-proposal/create-full-proposal/create-full-proposal.component.ts
+++ src/app/full-proposal/create-full-proposal/create-full-proposal.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
+
+import { environment } from '../../../environments/environment';
+
+import { AuthService } from '../../auth/auth.service';
 
 @Component({
   selector: 'app-create-full-proposal',
@@ -7,9 +14,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateFullProposalComponent implements OnInit {
 
-  constructor() { }
+  API_URL: string;
+
+  CreateShort: boolean;
+
+  constructor(private http: HttpClient,
+    public authService: AuthService, public dialogRef: MatDialogRef<CreateFullProposalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private router: Router) {
+
+    if (!environment.production) {
+      this.API_URL = environment.API_URL;
+    }
+    else {
+      this.API_URL = this.authService.getBackendURL();
+      console.log('this.API_URL', this.API_URL)
+    }
+
+    console.log('this.API_URL', this.API_URL)
+
+
+  }
 
   ngOnInit() {
   }
+
+  createFPFull() {
+
+    //route to the full Full proposal create
+
+    this.router.navigate(['/create-fp-full']);
+
+  }
+
+  createFPShort() {
+
+    this.CreateShort = true;
+
+  }
+
 
 }
