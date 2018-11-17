46df131eef29b4bb37b6f2493bdb2057ee513db9
Logan Born <lcborn4@gmail.com>
2018-11-17 18:12:46 -0500

    created the create-full-proposal-item component

---
+++ src/app/full-proposal/create-full-proposal-item/create-full-proposal-item.component.ts
@@ -0,0 +1,17 @@
+//modal component
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-full-proposal-item',
+  templateUrl: './create-full-proposal-item.component.html',
+  styleUrls: ['./create-full-proposal-item.component.css']
+})
+export class CreateFullProposalItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
