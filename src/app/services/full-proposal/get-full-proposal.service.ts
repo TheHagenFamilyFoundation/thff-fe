a8c07dfca97543d1df031660c4b7cf34944d6713
Logan Born <lcborn4@gmail.com>
2018-11-17 18:11:58 -0500

    added the getFullProposalByID function

--- src/app/services/full-proposal/get-full-proposal.service.ts
+++ src/app/services/full-proposal/get-full-proposal.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+import { AuthService } from '../../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GetFullProposalService {
 
-  constructor() { }
+  API_URL: string;
+
+  constructor(private http: HttpClient, private authService: AuthService, ) {
+
+    if (!environment.production) {
+      this.API_URL = environment.API_URL;
+    }
+    else {
+      this.API_URL = this.authService.getBackendURL();
+      console.log('this.API_URL', this.API_URL)
+    }
+
+    console.log('this.API_URL', this.API_URL)
+
+  }
+
+  getFullProposalByID(fpID: string): Observable<any> {
+
+    let urlString = this.API_URL + "/fullproposal?fpID=" + fpID;
+
+    return this.http.get(urlString);
+  }
+
+
 }
