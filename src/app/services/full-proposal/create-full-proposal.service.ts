16a8d5f30640284d43533157ef2590b992210681
Logan Born <lcborn4@gmail.com>
2018-11-17 18:12:08 -0500

    create full proposal route function

--- src/app/services/full-proposal/create-full-proposal.service.ts
+++ src/app/services/full-proposal/create-full-proposal.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+import { AuthService } from '../../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CreateFullProposalService {
 
-  constructor() { }
+  API_URL: string;
+
+  data: any;
+
+  constructor(private http: HttpClient, private authService: AuthService, ) {
+
+    if (!environment.production) {
+      this.API_URL = environment.API_URL;
+    }
+    else {
+      this.API_URL = this.authService.getBackendURL();
+      console.log('this.API_URL', this.API_URL)
+    }
+
+    console.log('this.API_URL', this.API_URL)
+
+  }
+
+  createFP(fp): Observable<any> {
+
+    let data = fp;
+
+    console.log('data', data)
+
+    return this.http.post(this.API_URL + '/fullproposal', data)
+  }
 }
