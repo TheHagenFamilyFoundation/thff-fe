69f089c6b5fe0ba408d7f3057819f52cc10f3116
Logan Born <lcborn4@gmail.com>
2019-03-03 14:23:03 -0500

    created director lois and organization routing

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -35,6 +35,8 @@ import { CreateFullProposalFullComponent } from './full-proposal/create-full-pro
 import { UserComponent } from './user/user.component';
 
 import { DirectorsMenuComponent } from './director/directors-menu/directors-menu.component';
+import { DirectorOrganizationsComponent } from './director/director-organizations/director-organizations.component';
+import { DirectorLoisComponent } from './director/director-lois/director-lois.component';
 import { DirectorViewOrganizationComponent } from './director/director-organizations/director-view-organization/director-view-organization.component';
 import { DirectorOrgViewLetterOfIntentComponent } from './director/director-organizations/director-view-organization/director-view-organization-lois/director-org-view-letter-of-intent/director-org-view-letter-of-intent.component';
 
@@ -90,6 +92,9 @@ const appRoutes: Routes = [
     { path: 'create-fp-full/:orgID/:loiID', component: CreateFullProposalFullComponent, canActivate: [AuthGuard] },
 
     { path: 'director', component: DirectorsMenuComponent, canActivate: [AuthGuard] },
+    { path: 'director/organizations', component: DirectorOrganizationsComponent, canActivate: [AuthGuard] },
+    { path: 'director/lois', component: DirectorLoisComponent, canActivate: [AuthGuard] },
+    { path: 'director/users', component: DirectorsMenuComponent, canActivate: [AuthGuard] },
     { path: 'director-organization/:id', component: DirectorViewOrganizationComponent, canActivate: [AuthGuard] },
     { path: 'director-loi/:id', component: DirectorOrgViewLetterOfIntentComponent, canActivate: [AuthGuard] },
 
