b19406cbf54f3679bae1b8df9f17801583be549b
Logan Born <lcborn4@gmail.com>
2019-03-06 21:49:57 -0500

    fixed the spec

--- src/app/director/director-organizations/director-view-organization/director-view-organization-lois/director-org-view-letter-of-intent/director-org-loi-voting/director-org-loi-voting.component.spec.ts
+++ src/app/director/director-organizations/director-view-organization/director-view-organization-lois/director-org-view-letter-of-intent/director-org-loi-voting/director-org-loi-voting.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DirectorOrgLoiVotingComponent } from './director-org-loi-voting.component';
 
-describe('DirectorOrgVotingComponent', () => {
+describe('DirectorOrgLoiVotingComponent', () => {
   let component: DirectorOrgLoiVotingComponent;
   let fixture: ComponentFixture<DirectorOrgLoiVotingComponent>;
 
