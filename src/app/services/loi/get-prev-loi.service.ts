4cafbe8a0548a87f8d65f38a14839858b289d0ec
Logan Born <lcborn4@gmail.com>
2019-03-09 15:33:16 -0500

    get prev loi service

---
+++ src/app/services/loi/get-prev-loi.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+import { AuthService } from '../../auth/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetPrevLoiService {
+
+  API_URL: string;
+
+  constructor(private http: HttpClient, private authService: AuthService, ) {
+
+    if (!environment.production) {
+      this.API_URL = environment.API_URL;
+    }
+    else {
+      this.API_URL = this.authService.getBackendURL();
+      console.log('this.API_URL', this.API_URL)
+    }
+
+    console.log('this.API_URL', this.API_URL)
+
+  }
+
+  getPrevLOI(ts: string): Observable<any> {
+
+    let urlString = this.API_URL + "/loi?ts=" + ts;
+
+    return this.http.get(urlString);
+  }
+
+}
