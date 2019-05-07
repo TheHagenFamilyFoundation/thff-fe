c0fe9978b8391b2c90bbc2de939fb0786a34f473
Logan Born <lcborn4@gmail.com>
2019-05-06 21:22:12 -0400

    created the loi list component - printable

---
+++ src/app/director/director-lois/loi-list/loi-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loi-list',
+  templateUrl: './loi-list.component.html',
+  styleUrls: ['./loi-list.component.css']
+})
+export class LoiListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
