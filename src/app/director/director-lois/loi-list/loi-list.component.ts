1ea1a63583119fbdae74ecaa99f5fab0fb3828db
Logan Born <lcborn4@gmail.com>
2019-05-06 23:44:41 -0400

    just an output for printable

--- src/app/director/director-lois/loi-list/loi-list.component.ts
+++ src/app/director/director-lois/loi-list/loi-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+// import { GetLoiService } from '../../../services/loi/get-loi.service';
+// import { LOIStatusService } from '../../../services/loi/loi-status.service';
 
 @Component({
   selector: 'app-loi-list',
@@ -7,9 +11,67 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoiListComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  lois: any;
+
+  // lois: any
+
+  Loaded: boolean;
+
+  constructor(
+    // public getLoiService: GetLoiService,
+    // private loiStatusService: LOIStatusService
+  ) {
+    this.Loaded = false;
+  }
 
   ngOnInit() {
+
+    // this.getLOIs();
+
   }
+
+  // getLOIs() {
+
+  //   this.getLoiService.getAllLOIs()
+  //     .subscribe(
+  //       (lois) => {
+
+  //         console.log('lois', lois);
+
+  //         this.lois = lois;
+
+  //         this.setStatuses();
+
+  //         this.Loaded = true;
+
+  //       })
+
+  // }
+
+  // setStatuses() {
+
+  //   console.log('setting status')
+
+  //   this.lois.forEach(loi => {
+
+  //     console.log('before LOI', loi)
+
+  //     console.log('status', loi.status)
+
+  //     loi.status = this.configureStatus(loi.status);
+
+  //     console.log('after LOI', loi)
+
+  //   });
+
+  // }
+
+  //takes in a status s that is a number
+  // configureStatus(s: number): string {
+
+  //   return this.loiStatusService.getStatus(s)
+
+  // }
 
 }
