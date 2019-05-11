f12eced035f4b9a343e4da1c499431c30c98d41e
Logan Born <lcborn4@gmail.com>
2019-05-11 17:54:40 -0400

    export to pdf

--- src/app/director/director-lois/loi-list/loi-list.component.ts
+++ src/app/director/director-lois/loi-list/loi-list.component.ts
@@ -42,109 +42,47 @@ export class LoiListComponent implements OnInit {
     console.log('export to pdf pressed')
 
     // var pdf = new jsPDF();
-        var pdf = new jsPDF('p', 'pt', 'letter');
+    var pdf = new jsPDF('p', 'mm', 'a4');
     // var pdf = new jsPDF('p', 'pt', 'a0');
     let promises = [];
 
+
+    //this is the title
     // pdf.setFontSize(40)
-    pdf.setFontSize(18)
-    pdf.text(100, 10, 'List of Letter of Intents')
+    pdf.setFontSize(12)
+    pdf.text(75, 10, 'List of Letter of Intents')
 
 
     this.lois.forEach(loi => {
 
-      // let loiDoc = '#loi_' + this.lois[0].id;
-      // let loiDoc = '#loi1';
       let loiDoc = '#loi_' + loi.id;
       console.log('loiDoc', loiDoc)
-
-      // parentdiv is the html element which has to be converted to PDF
-      // html2canvas(document.querySelector(loiDoc)).then(canvas => {
-
-      //   console.log("canvas", canvas)
-      //   console.log("canvas.width", canvas.width)
-      //   console.log("canvas.height", canvas.height)
-
-      //   // var pdf = new jsPDF('p', 'pt', [canvas.width, canvas.height]);
-
-      //   var imgData = canvas.toDataURL("image/jpeg", 1.0);
-      //   console.log('imgData', imgData)
-      // pdf.addImage(imgData, 15, 100, canvas.width, canvas.height);
-      //   // pdf.addImage(imgData, 'JPEG', 15, 40, 180, 160)
-      //   pdf.save('converteddoc.pdf');
-
-      // });
-
-
 
       promises.push(html2canvas(document.querySelector(loiDoc)))
 
     });
-    // html2canvas(document.querySelector(loiDoc)).then((canvas) => {
+
     Promise.all(promises).then((canvases) => {
 
       canvases.forEach((canvas, index) => {
 
-        console.log("canvas", canvas)
-        console.log("canvas.width", canvas.width)
-        console.log("canvas.height", canvas.height)
+        var imgWidth = 208; //default 208
+        var pageHeight = 500; //295
+        var imgHeight = canvas.height * imgWidth / canvas.width;
+        var heightLeft = imgHeight;
+        var position = 30;
 
         var imgData = canvas.toDataURL("image/jpeg", 1.0);
-        console.log("imgData", imgData)
-        pdf.addImage(imgData, 15, 100, canvas.width, canvas.height);
+        pdf.addImage(imgData, 'PNG', 0, position, imgWidth, imgHeight)
 
         if (index < this.lois.length - 1) {
-          //debug
-          // if (index < 0) {
           pdf.addPage();
         }
 
-        // pdf.save('converteddoc.pdf');
       });
 
       pdf.save('converteddoc.pdf');
     })
-
-
-
-    // parentdiv is the html element which has to be converted to PDF
-    // html2canvas(document.querySelector("#parentdiv")).then(canvas => {
-
-    //   // var pdf = new jsPDF('p', 'pt', [canvas.width, canvas.height]);
-    //   console.log("canvas", canvas)
-    //   console.log("canvas.width", canvas.width)
-    //   console.log("canvas.height", canvas.height)
-
-    //   let pageHeight = 0;
-
-    //   console.log('pageHeight', pageHeight)
-
-    //   if (pageHeight > 0) {
-    //     pdf.addPage();
-    //   }
-
-    // var imgData = canvas.toDataURL("image/jpeg", 1.0);
-    // pdf.addImage(imgData, 0, 0, canvas.width, pageHeight);
-
-
-    // pageHeight += 500;
-
-    // var pageHeight = pdf.internal.pageSize.height;
-    // console.log("pageHeight", pageHeight)
-
-    // var doc = new jsPDF();
-    // var pageHeight = doc.internal.pageSize.height;
-    // console.log('pageHeight', pageHeight)
-
-    // Before adding new content
-    // var y = 500 // Height position of new content
-    // if (y >= pageHeight) {
-    //   doc.addPage();
-    //   y = 0 // Restart height position
-    // }
-    // doc.text(x, y, "value");
-
-
 
   }
 
