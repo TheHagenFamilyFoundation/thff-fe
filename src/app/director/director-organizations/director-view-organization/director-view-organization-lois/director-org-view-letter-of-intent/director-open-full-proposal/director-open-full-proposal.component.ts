a2e8b4a330de64e7698c9d17fd96a32e0e4ce384
Logan Born <lcborn4@gmail.com>
2019-05-18 23:15:57 -0400

    input the loi

--- src/app/director/director-organizations/director-view-organization/director-view-organization-lois/director-org-view-letter-of-intent/director-open-full-proposal/director-open-full-proposal.component.ts
+++ src/app/director/director-organizations/director-view-organization/director-view-organization-lois/director-org-view-letter-of-intent/director-open-full-proposal/director-open-full-proposal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-director-open-full-proposal',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./director-open-full-proposal.component.css']
 })
 export class DirectorOpenFullProposalComponent implements OnInit {
+
+  @Input()
+  loi: any;
+  loiID: any;
 
   constructor() { }
 
