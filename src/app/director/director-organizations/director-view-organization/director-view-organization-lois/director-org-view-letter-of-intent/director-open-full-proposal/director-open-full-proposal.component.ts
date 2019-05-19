471f3c6cf1b4187a463c07e50702dd1b1561847e
Logan Born <lcborn4@gmail.com>
2019-05-19 14:16:05 -0400

    call the open fp and also close fp if need be

--- src/app/director/director-organizations/director-view-organization/director-view-organization-lois/director-org-view-letter-of-intent/director-open-full-proposal/director-open-full-proposal.component.ts
+++ src/app/director/director-organizations/director-view-organization/director-view-organization-lois/director-org-view-letter-of-intent/director-open-full-proposal/director-open-full-proposal.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+import { OpenLoiFpService } from './../../../../../../services/full-proposal/open-loi-fp.service';
+
 @Component({
   selector: 'app-director-open-full-proposal',
   templateUrl: './director-open-full-proposal.component.html',
@@ -9,11 +11,62 @@ export class DirectorOpenFullProposalComponent implements OnInit {
 
   @Input()
   loi: any;
-  loiID: any;
+  loiID: any; //5 character
 
-  constructor() { }
+  Opened: boolean;
+
+  constructor(public openLoiFpService: OpenLoiFpService) {
+
+    this.Opened = false;
+
+  }
 
   ngOnInit() {
+
+    console.log('open full proposal - loi', this.loi)
+
+    this.checkOpenFP();
+
   }
+
+  openFullProposal(data) {
+
+    console.log('openFullProposal', data) //debug
+
+    console.log('this.loi', this.loi)
+
+    let body = {
+      id: this.loi.id,
+      open: data
+    }
+
+    console.log('open - body', body)
+
+    this.openLoiFpService.openFPs(body).subscribe(
+      (loi) => {
+        console.log('return - loi', loi)
+        this.loi = loi;
+        this.loiID = loi.loiID
+
+        this.checkOpenFP();
+
+
+      },
+      (err) => {
+        console.log(err)
+      }
+    )
+
+  }
+
+  checkOpenFP() {
+    if (this.loi.openFp == true) {
+      this.Opened = true;
+    }
+    else {
+      this.Opened = false;
+    }
+  }
+
 
 }
