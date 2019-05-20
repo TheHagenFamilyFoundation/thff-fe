52d5638b804d11ff037d968ddfed054d5de70c75
Logan Born <lcborn4@gmail.com>
2019-05-20 00:18:40 -0400

    get submission years

--- src/app/admin/admin-full-proposal/admin-full-proposal.component.ts
+++ src/app/admin/admin-full-proposal/admin-full-proposal.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 import { OpenFpPortalService } from './../../services/full-proposal/open-fp-portal.service';
-
-
+import { SubmissionYearService } from './../../services/admin/submission-year.service';
 
 @Component({
   selector: 'app-admin-full-proposal',
@@ -16,9 +15,20 @@ export class AdminFullProposalComponent implements OnInit {
 
   accessLevel: number;
 
+  Opened: boolean;
+
+  submissionyears = [];
+  SubmissionYear: boolean;
+  currentSY: any; //toggleable
+
   constructor(
-    public openFpPortalService: OpenFpPortalService
-  ) { }
+    public openFpPortalService: OpenFpPortalService,
+    public submissionYearService: SubmissionYearService
+  ) {
+
+    this.Opened = false;
+
+  }
 
   ngOnInit() {
 
@@ -26,10 +36,128 @@ export class AdminFullProposalComponent implements OnInit {
 
     this.accessLevel = this.user.accessLevel
 
-  }
-
-  openFullProposalPortal() {
+    this.getSubmissionYears();
 
   }
+
+  getSubmissionYears() {
+
+    this.submissionYearService.getSubmissionYears().subscribe(
+      (sys) => {
+
+        console.log('get submission years', sys)
+
+        let noSubmissionYear = false;
+
+
+        sys.forEach(sy => {
+
+          if (this.checkCurrentYear(sy.year)) {
+            noSubmissionYear = true;
+
+            this.currentSY = sy;
+          }
+
+        });
+
+        if (noSubmissionYear) {
+
+          //show the message saying no submission year has been created
+
+          this.SubmissionYear = true;
+
+          if (this.currentSY.fpPortal) {
+            this.Opened = false;
+          }
+
+
+        }
+
+
+      },
+      (err) => {
+        console.log('err', err)
+      }
+    )
+
+  }
+
+  openFullProposalPortal(data) {
+
+    //temp - get current year
+    //possibly show an array of the years
+    let today = new Date();
+    let currentYear = today.getFullYear();
+
+    let body = {
+      open: data,
+      sy: this.currentSY
+    }
+
+    //debug
+    console.log('openFullProposalPortal', body)
+
+    this.openFpPortalService.openFPs(body).subscribe(
+      (lois) => {
+
+        console.log('portals are open lois', lois)
+
+        this.getSubmissionYear(this.currentSY.id)
+
+      },
+      (err) => {
+        console.log('err', err)
+      }
+
+
+    )
+
+
+  }
+
+  // checkOpenFPportal() {
+  //   if (this.loi.openFp == true) {
+  //     this.Opened = true;
+  //   }
+  //   else {
+  //     this.Opened = false;
+  //   }
+  // }
+
+  checkCurrentYear(year) {
+
+    let today = new Date();
+    let currentYear = today.getFullYear();
+
+    return year === currentYear;
+  }
+
+  getSubmissionYear(id) {
+    //debug
+    console.log('getSubmissionYear', id)
+
+    //return 1 submission year
+    this.submissionYearService.getSubmissionYear(id).subscribe(
+      (sy) => {
+
+        console.log('setting currentSY', sy)
+        this.currentSY = sy;
+
+        if (this.currentSY.fpPortal) {
+          this.Opened = true;
+        }
+        else {
+          this.Opened = false;
+        }
+
+        console.log('after getting SY - this.Opened', this.Opened)
+
+
+      },
+      (err) => {
+        console.log('err', err)
+      })
+  }
+
 
 }
