f2939d93c7fe64ad19a40bf310ed0840bf0a28bc
Logan Born <lcborn4@gmail.com>
2019-06-08 11:54:46 -0400

    view the full proposal item before deleting

--- src/app/full-proposal/delete-full-proposal-item/delete-full-proposal-item.component.ts
+++ src/app/full-proposal/delete-full-proposal-item/delete-full-proposal-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-delete-full-proposal-item',
@@ -7,9 +8,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DeleteFullProposalItemComponent implements OnInit {
 
-  constructor() { }
+  catDescriptionLength: string;
+
+  catDescription: string; //Category/Description
+  amountRequestedTHFF: string;
+  amountRequested: string;
+  amountPending: string;
+  total: string;
+
+  constructor(public dialogRef: MatDialogRef<DeleteFullProposalItemComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any, ) {
+
+    console.log('data', data)
+
+    this.catDescriptionLength = '0'; //default
+
+    this.catDescription = data.fpItem.categoryDescription;
+    this.catDescriptionLength = data.fpItem.categoryDescription.length;
+    this.amountRequestedTHFF = data.fpItem.amountRequestedTHFF;
+    this.amountRequested = data.fpItem.amountRequested;
+    this.amountPending = data.fpItem.amountPending;
+    this.total = data.fpItem.total;
+
+  }
 
   ngOnInit() {
+  }
+
+  deleteFPItem() {
+    console.log('deleteFPItem')
+
+    console.log('catDescription', this.catDescription)
+
+    var body = {
+      delete: true
+    }
+
+    this.dialogRef.close(body);
+
+  }
+
+  cancel() {
+
+    console.log('cancel pressed');
+
   }
 
 }
