887617109ee6d92a365930e0a93b420a98f460d5
Logan Born <lcborn4@gmail.com>
2019-06-08 14:20:30 -0400

    edit full proposal created

---
+++ src/app/full-proposal/edit-full-proposal-item/edit-full-proposal-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-full-proposal-item',
+  templateUrl: './edit-full-proposal-item.component.html',
+  styleUrls: ['./edit-full-proposal-item.component.css']
+})
+export class EditFullProposalItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
