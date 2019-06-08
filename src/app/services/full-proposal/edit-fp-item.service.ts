fd452dc3cdaff277ab3f62af75f5899fdc623623
Logan Born <lcborn4@gmail.com>
2019-06-08 17:13:19 -0400

    edit the fp item

--- src/app/services/full-proposal/edit-fp-item.service.ts
+++ src/app/services/full-proposal/edit-fp-item.service.ts
@@ -1,9 +1,38 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
+
+import { AuthService } from '../../auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EditFpItemService {
 
-  constructor() { }
+  API_URL: string;
+
+  data: any;
+
+  constructor(private http: HttpClient, private authService: AuthService, ) {
+
+    if (!environment.production) {
+      this.API_URL = environment.API_URL;
+    }
+    else {
+      this.API_URL = this.authService.getBackendURL();
+      console.log('this.API_URL', this.API_URL)
+    }
+
+    console.log('this.API_URL', this.API_URL)
+
+  }
+
+  editFPItem(body): Observable<any> {
+
+    console.log('body', body)
+
+    return this.http.patch(this.API_URL + `/fullproposalitem/${body.id}`, body)
+  }
+
 }
