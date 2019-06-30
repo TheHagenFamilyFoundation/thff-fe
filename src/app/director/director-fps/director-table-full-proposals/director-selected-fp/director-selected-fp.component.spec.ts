dc635f809309890f14f255133a76b9fbd4801a91
Logan Born <lcborn4@gmail.com>
2019-06-29 21:59:34 -0400

    uppercase the FP

--- src/app/director/director-fps/director-table-full-proposals/director-selected-fp/director-selected-fp.component.spec.ts
+++ src/app/director/director-fps/director-table-full-proposals/director-selected-fp/director-selected-fp.component.spec.ts
@@ -2,7 +2,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DirectorSelectedFPComponent } from './director-selected-fp.component';
 
-describe('DirectorSelectedFpComponent', () => {
+describe('DirectorSelectedFPComponent', () => {
   let component: DirectorSelectedFPComponent;
   let fixture: ComponentFixture<DirectorSelectedFPComponent>;
 
