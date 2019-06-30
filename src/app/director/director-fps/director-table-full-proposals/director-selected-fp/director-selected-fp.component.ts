852754c5785adccdb582da39a848ca1bd5df8f36
Logan Born <lcborn4@gmail.com>
2019-06-29 22:33:45 -0400

    route to the director view fp

--- src/app/director/director-fps/director-table-full-proposals/director-selected-fp/director-selected-fp.component.ts
+++ src/app/director/director-fps/director-table-full-proposals/director-selected-fp/director-selected-fp.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-director-selected-fp',
@@ -7,9 +9,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DirectorSelectedFPComponent implements OnInit {
 
-  constructor() { }
+  fpLink = '/director-fp/'
+  link: string;
+
+  fpID: any;
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: any,
+    private router: Router,
+  ) { }
 
   ngOnInit() {
+    // will log the entire data object
+    console.log('this.data', this.data)
+
+    this.fpID = this.data.fpID;
+
+    this.link = this.fpLink + this.fpID;
+
+    console.log('this is the link', this.link);
+
+  }
+
+  cancel() {
+
+    console.log('cancel pressed');
+
   }
 
 }
