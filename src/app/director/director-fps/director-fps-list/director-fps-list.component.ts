efa0db4628e25c6a3107fb4c687e0a37cbffe7ce
Logan Born <lcborn4@gmail.com>
2019-06-30 20:55:29 -0400

    created the fp list

---
+++ src/app/director/director-fps/director-fps-list/director-fps-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-director-fps-list',
+  templateUrl: './director-fps-list.component.html',
+  styleUrls: ['./director-fps-list.component.css']
+})
+export class DirectorFpsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
