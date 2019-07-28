8742bd43ecd4aaf78806c2be12dbe0753f77414d
Logan Born <lcborn4@gmail.com>
2019-07-27 20:28:59 -0400

    added comment

--- src/app/organization/organization-users/add-users/add-users.component.ts
+++ src/app/organization/organization-users/add-users/add-users.component.ts
@@ -242,7 +242,7 @@ export class AddUsersComponent implements OnInit {
 
         // console.log('we have user', this.dataSourceAllUsers.data);
         // this.dataSourceAllUsers = new MatTableDataSource(this.dataSourceAllUsers.data);
-        this.allUsersPaginator.length = this.usersCount--;
+        this.allUsersPaginator.length = this.usersCount--; //reduce the count after selecting 1
         this.dataSourceAllUsers.paginator = this.allUsersPaginator;
         this.dataSourceAllUsers.sort = this.allUsersSort;
 
