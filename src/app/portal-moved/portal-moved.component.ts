4592078723312ed967f9c97568dbcb1082c5ee86
Logan Born <lcborn4@gmail.com>
2020-03-01 18:33:22 -0500

    portal moved component

---
+++ src/app/portal-moved/portal-moved.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portal-moved',
+  templateUrl: './portal-moved.component.html',
+  styleUrls: ['./portal-moved.component.css']
+})
+export class PortalMovedComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
